f9187369c20a94d4bcae8b7a1cc6f1338e3db0ed
Yeechan Lu <wz.bluesnow@gmail.com>
2014-04-28 17:31:18 +0800

    Remove useless spec

--- spec/ripple-rest_spec.rb
+++ spec/ripple-rest_spec.rb
@@ -1,7 +1,5 @@
 require "spec_helper"
 
-describe NewGem::Awesome do
-  it "is awesome" do
-    NewGem::Awesome.new.describe.should be_awesome
-  end
+describe RippleRest do
+  
 end
--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -1,13 +1,6 @@
 module RippleRest
   module Spec
     module Matchers
-      def be_awesome
-        Rspec::Matchers::Matcher.new :be_awesome do
-          match do |actual|
-            actual.should == "awesome"
-          end
-        end
-      end
     end
   end
 end
